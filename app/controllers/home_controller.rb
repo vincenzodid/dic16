b4a51452a7e885e4365a4bcb1ea07f35c6682305
vincenzodid <v.didonna@donboscosandona.it>
2021-12-16 09:08:15 +0000

    applicazione iniziale

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+    #questa sarà la Home page
+    def  index
+        
+    end
+    #questa sarà la pagina chi siamo
+    def chi_siamo 
+        
+    end
+end
